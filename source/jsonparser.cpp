37807649acecdf80d24161fa896ea6a17a7a739a
Rafael Almeida <ra_fa_94el@hotmail.com>
2014-05-13 17:28:05 +0000

    jsonparser.cpp edited online with Bitbucket

--- source/jsonparser.cpp
+++ source/jsonparser.cpp
@@ -1,1 +1,41 @@
 // parser json (trabalho inicial: Rafael Almeida) gramática reconhecedora de param-list.json
+
+%{
+    #include <stdio.h>
+    int yylex(void);
+    int yyerror(char *s);
+%}
+
+%%
+
+File :	'{' PL '}'									         	   // File = param-list.json
+	 ;															   // PL = parameter list
+														           // PI = parameter item
+ 														           // Pk = parameter key
+																   // PAL = parameter atribute list
+PL   :	/* lambda */											   // AT = atribute
+	 |  PI ',' PL
+	 ;
+	
+PI   : PK '{' PAL '}'
+	 ;
+	 
+PK   : id                                                          // reconhecido e fornecido pelo lexer2 (flex)
+	 ;
+	 
+PAL  : /* lambda */
+	 | AT ',' PAL
+	 ;
+	 
+AT   : /* lambda */
+	 | "comment" ":" 
+	 | "class" ":"
+	 | "value type" ":"
+	 | "XML name" ":"
+	 | "default value" ":"
+	 ;
+	 
+
+
+	 
+	 
