d8a8d0b21a5382c97107ecc2d1bad440e2ab09a5
Rodrigo Cunha <RodrigoCunha@aluno-1806.wireless.ua.pt>
2014-06-04 15:28:49 +0100

    URDF radius constant

--- source/classes/Beacons.cpp
+++ source/classes/Beacons.cpp
@@ -34,7 +34,7 @@ void Beacons::URDFOutput(ofstream& file){
     int count = 1;
     
     for (vector<Beacon>::iterator it = beacons->begin() ; it != beacons->end(); ++it){
-        file << "\t<link name=\""<< "beacon" << count++ <<"\">\n\t\t<visual>\n\t\t\t<origin xyz=\"" << (*it).position.getX() << " "<< (*it).position.getY() << " "<< (*it).height <<"\"/>\n\t\t\t<geometry>\n\t\t\t\t<sphere radius=\"" << (*it).radius << "\"/>\n\t\t\t</geometry>\n\t\t\t<material name=\"Yellow2\">\n\t\t\t\t<color rgba=\"0.8 0.8 0 1.0\"/>\n\t\t\t</material>\n\t\t</visual>\n\t</link>\n";
+        file << "\t<link name=\""<< "beacon" << count++ <<"\">\n\t\t<visual>\n\t\t\t<origin xyz=\"" << (*it).position.getX() << " "<< (*it).position.getY() << " "<< (*it).height <<"\"/>\n\t\t\t<geometry>\n\t\t\t\t<sphere radius=\"0.3\"/>\n\t\t\t</geometry>\n\t\t\t<material name=\"Yellow2\">\n\t\t\t\t<color rgba=\"0.8 0.8 0 1.0\"/>\n\t\t\t</material>\n\t\t</visual>\n\t</link>\n";
         
     }
     
