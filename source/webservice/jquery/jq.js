5bf15105b8dbe6811a0a1b890a8bfe16e4d0eb2c
Rafael Ferreira <mail@rafaelferreira.pt>
2014-06-05 00:05:20 +0100

    jquery

--- source/webservice/jquery/jq.js
+++ source/webservice/jquery/jq.js
@@ -4,12 +4,15 @@ $( document ).ready(function() {
 		var challange = $('#challange').val();
 		$('#output').text('Please wait a moment!');
 		$.post("compiling.php",{param: param, challange: challange}, function(data){
+			$('#output').text(data);
+			/*
 			var response = jQuery.parseJSON(data);
 			if(typeof response =='object'){
 				$('#grid').text(response[0]);
 			}else{
 				$('#output').text(data);
 			}
+			*/
 		});
     }); 
 });
