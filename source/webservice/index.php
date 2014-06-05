a888c6a940c821e58890341db9f27af2ee971743
Rafael Ferreira <mail@rafaelferreira.pt>
2014-06-05 14:00:12 +0100

    #980000

--- source/webservice/index.php
+++ source/webservice/index.php
@@ -34,6 +34,7 @@
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
             <li class="active"><a href="index.php">Json to XML</a></li>
+            <li class="active"><a href="api.php" style="color: #980000">API</a></li>
             <li><a href="http://mymodelrobot.appspot.com/" target="_blank">URDF viewer</a></li>
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">Membros <b class="caret"></b></a>
