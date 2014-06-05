57751020b7786ce3f02ef818dad007d4527eb1bb
Rafael Ferreira <mail@rafaelferreira.pt>
2014-06-05 01:21:12 +0100

    bernardo não tem face LOL

--- source/webservice/index.php
+++ source/webservice/index.php
@@ -39,7 +39,7 @@
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">Membros <b class="caret"></b></a>
               <ul class="dropdown-menu">
                 <li><a href="http://www.rafaelferreira.pt/">Rafael Ferreira</a></li>
-                <li><a href="https://www.facebook.com/Bernardo94?fref=ts">Bernardo Ferreira</a></li>
+                <li><a href="#">Bernardo Ferreira</a></li>
                 <li><a href="https://www.facebook.com/rlopescunha">Rodrigo Cunha</a></li>
                 <li><a href="https://www.facebook.com/profile.php?id=100000144346090">Rafael Almeida</a></li>
                 <li><a href="https://www.facebook.com/quintanilha.joao.9">João Quintanilha</a></li>
