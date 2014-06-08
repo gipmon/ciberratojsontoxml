6e52c5a5b2c01ecbd82e338939bb0f40bd921e13
Rafael Ferreira <mail@rafaelferreira.pt>
2014-06-08 11:12:01 +0100

    rel="select"

--- source/webservice/jquery/jq.js
+++ source/webservice/jquery/jq.js
@@ -22,6 +22,26 @@ $( document ).ready(function() {
 			append: false
 		});
 	}
+	function SelectText(element) {
+	    var text = document.getElementById(element);
+	    if ($.browser.msie) {
+	        var range = document.body.createTextRange();
+	        range.moveToElementText(text);
+	        range.select();
+	    } else if ($.browser.mozilla || $.browser.opera) {
+	        var selection = window.getSelection();
+	        var range = document.createRange();
+	        range.selectNodeContents(text);
+	        selection.removeAllRanges();
+	        selection.addRange(range);
+	    } else if ($.browser.safari) {
+	        var selection = window.getSelection();
+	        selection.setBaseAndExtent(text, 0, text, 1);
+	    }
+	}
+	$("[rel=select]").click(function() {
+    	SelectText($(this));
+  	});
     $("#make").click(function(){ 
 		var param = $('#param').val();
 		var challenge = $('#challenge').val();
